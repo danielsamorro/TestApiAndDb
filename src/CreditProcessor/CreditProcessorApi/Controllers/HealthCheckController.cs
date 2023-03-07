4e1cab7d404ea4ef3300535371b99d1a74993dda
Daniel Matos Samorro <dms.samorro@hotmail.com>
2023-03-07 14:24:27 -0300

    added initial app structure

---
+++ src/CreditProcessor/CreditProcessorApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CreditProcessorApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet] public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
